92749f331c34668e1ec7167f8600498030d2f0a1
tdtc-hrb <tdtc.hrb@gmail.com>
2026-01-06 22:43:23 -0800

    Declaring a class as final prevents it from being subclassed—period; it’s the end of the line.

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class AboutController extends AbstractController
+final class AboutController extends AbstractController
 {
     #[Route('/about', name: 'app_about')]
     public function index(): Response
